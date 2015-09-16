b5f359587745d72e6ddedb5356cb8cf8912326ec
McNight <mcnight@mcnight.fr>
2015-09-16 15:20:15 +0200

    Initial Commit

---
+++ Paris TransitTests/Paris_TransitTests.swift
@@ -0,0 +1,36 @@
+//
+//  Paris_TransitTests.swift
+//  Paris TransitTests
+//
+//  Created by Adam McNight on 16/09/2015.
+//  Copyright © 2015 Vanadium Applications. All rights reserved.
+//
+
+import XCTest
+@testable import Paris_Transit
+
+class Paris_TransitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
